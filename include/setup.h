dab40ad7c80c10c7377372551812d9b9b77e233e
ishitani@ne.phys.sci.osaka-u.ac.jp <ishitanisoshi@dhcp-192-168-75-109.triumfvisitor.net>
2024-08-30 09:26:11 +0900

    Add ./include/setup.h

---
+++ include/setup.h
@@ -0,0 +1,19 @@
+// setup.h
+#include <cmath>
+
+#ifndef SETUP_H
+#define SETUP_H
+
+const double L0 = 31.5/2.0;
+const double L1 = 34.5/2.0;
+const double L2 = 111.5/2.0;
+const double L3 = 114.5/2.0;
+
+const double z0[2] = {-L0,L0};
+const double z1[2] = {-L1,L1};
+const double z2[2] = {-L2,L2};
+const double z3[2] = {-L3,L3};
+const double deg = 45.0;
+const double theta = M_PI * deg / 180.0;
+
+#endif
